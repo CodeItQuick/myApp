853671b94f8f3e25d684b36083460c0eb24f58a4
CodeItQuick <evanontario@hotmail.com>
2023-10-25 22:38:49 -0300

    Update and rename 10yearslater.js to 5yearslater.js

--- 5yearslater.js
+++ 5yearslater.js
@@ -6,3 +6,14 @@ var uniqueWords = new Set(words);
 var uniqueWordsArr = [...uniqueWords];
 var output = uniqueWordsArr.map(x => ({ [x]: words.filter(y => y === x).length}))
 console.log(output)
+// {Hello,: 1}
+// {I: 2}
+// {like: 4}
+// {cats: 3}
+// {Do: 1}
+// {you: 5}
+// {No: 1}
+// {Are: 2}
+// {sure: 2}
+// {Why: 1}
+// {don't: 1}
